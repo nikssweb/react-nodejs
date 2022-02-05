25719d3bc10c96b4184dad108b7686274c4dcc15
nikssweb <niksswebdev@gmail.com>
2022-02-05 21:33:47 +0530

    components added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import "./App.css";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to ShoPaying</h1>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ShoPaying</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
 
