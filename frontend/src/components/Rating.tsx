0816f60dfaddf30fe1dceed42303239d3be050ef
nikssweb <niksswebdev@gmail.com>
2022-03-05 23:36:08 +0530

    frontend basic setup done

---
+++ frontend/src/components/Rating.tsx
@@ -0,0 +1,79 @@
+type Props = {
+  value: number;
+  text: string;
+  color: string;
+};
+
+const Rating = (props: Props) => {
+  return (
+    <div className="rating">
+      <span>
+        <i
+          style={{ color: props.color }}
+          className={
+            props.value >= 1
+              ? "fas fa-star"
+              : props.value >= 0.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: props.color }}
+          className={
+            props.value >= 2
+              ? "fas fa-star"
+              : props.value >= 1.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: props.color }}
+          className={
+            props.value >= 3
+              ? "fas fa-star"
+              : props.value >= 2.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: props.color }}
+          className={
+            props.value >= 4
+              ? "fas fa-star"
+              : props.value >= 3.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color: props.color }}
+          className={
+            props.value >= 5
+              ? "fas fa-star"
+              : props.value >= 4.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>{props.text && props.text}</span>
+    </div>
+  );
+};
+
+Rating.defaultProps = {
+  color: "#f8e825",
+};
+
+export default Rating;
