274ed2ff57cdeb2322cf84ff88acb942c8f8dd1e
nikssweb <niksswebdev@gmail.com>
2022-04-07 15:42:52 +0530

    backend-setup

---
+++ backend/data/products.js
@@ -0,0 +1,43 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Bluetooth Headphones",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience. Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "iPhone 11 Pro 256GB Memory",
+    image: "/images/phone.jpg",
+    description:
+      "Introducing the iPhone 11 Pro. A transformative triple camera system that adds tons of cabability without complexity. An unprecedented leap in battery life",
+    brand: "Apple",
+    category: "Electronics",
+    price: 599.99,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    _id: "3",
+    name: "Cannon EOS 80D DSLR Camera",
+    image: "/images/camera.jpg",
+    description:
+      "Characterised by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intutive design",
+    brand: "Apple",
+    category: "Electronics",
+    price: 929.99,
+    countInStock: 5,
+    rating: 3.5,
+    numReviews: 3,
+  },
+];
+
+module.exports = products;
